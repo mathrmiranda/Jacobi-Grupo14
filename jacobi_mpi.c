88454c6eb9beb0ab981f1f232e0f417dadbf1455
Matheus Miranda <matheus.resende.miranda@usp.br>
2022-07-07 17:16:31 -0300

    Codigo mpi finalizado

--- jacobi_mpi.c
+++ jacobi_mpi.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "mpi.h"
+#include <mpi.h>
+#include <omp.h>
 
+#define ERRO_ACEITAVEL 0.00001
 
 int main(int argc, char *argv[]){
-    int npes, myrank, src, dest, msgtag, ret, tam, seed, i, j;
-    double *xk;
-    double somalinha = 0;
+    int npes, myrank, src, dest, msgtag, ret, tam, seed, i, j,k = 0, flag_parada = 0, n_threads;
+    double *xk,*xk_1;
+    double somalinha = 0, precnum = 0, precden = 0, precit;
 	double *bufrecv;
 
 	/**INICIALIZANDO MPI**/
@@ -22,57 +24,47 @@ int main(int argc, char *argv[]){
 	MPI_Get_processor_name(processor_name, &name_len);
 
 	/**RECEBENDO ARGUMENTOS**/
-	 if ( argc  != 2)
+	 if ( argc  != 3)
     {
-	printf("Argumentos errados. Utilize <ordem da matriz> \n");
+	printf("Argumentos errados. Utilize <ordem da matriz> <numero de threads>\n");
 	exit(0);
     }
 
     tam = atoi(argv[1]);
+    n_threads = atoi(argv[2]);
     seed = 2;
-    xk = (double*)malloc(tam*sizeof(double));
+    xk = (double*)malloc((tam)*sizeof(double));
+
+    //printf("Processo %d executando no processador %s\n", myrank, processor_name);
 
     /**PROCESSO 0**/
     if ( myrank == 0) {
-        printf("Existem %d processos.\n", npes);
-        printf("Eu sou o processo %d no processador %s. As matrizes A e b sao as seguintes \n", myrank, processor_name);
-
-        /**INICIALIZANDO E PREENCHENDO AS MATRIZES A E b**/
+        
+        /**INICIALIZANDO E PREENCHENDO AS MATRIZES S**/
         double S[tam][tam+1];
 
         srand(seed);
 
-        for(i=0;i<tam;i++)
-        {
-                for(j=0;j<tam;j++)
-                {
-                    if(i != j)
-                    {
-                        S[i][j] = rand() %100;
-                        somalinha += S[i][j];
-                    }
-                }
-            S[i][i] = somalinha + (rand()%10) + 1; //Garante convergencia
-            S[i][tam] = rand() % 100;
-            somalinha = 0;
-        }
-
-        for(i=0;i<tam;i++){
-            xk[i] = S[i][tam]/S[i][i];
-            printf("%.1f ",xk[i]);
-        }
-
-        //Imprimindo S
-        printf("\nS = \n");
-        for(i=0;i<tam;i++)
-        {
-            for(j=0;j<tam+1;j++)
+        #pragma omp parallel for num_threads(n_threads) private(i,j,somalinha) shared(S,tam)
+            for(i=0;i<tam;i++)
             {
-                printf("%.1f ", S[i][j]);
+                    for(j=0;j<tam;j++)
+                    {
+                        if(i != j)
+                        {
+                            S[i][j] = rand() %10;
+                            somalinha += S[i][j];
+                        }
+                    }
+                S[i][i] = somalinha + (rand()%10) + 1; //Garante convergencia
+                S[i][tam] = rand() % 100;
+                somalinha = 0;
             }
-            printf("\n");
-        }
-        printf("\n");
+
+
+            for(i=0;i<tam;i++){
+                xk[i] = S[i][tam]/S[i][i]; //Vetor x de resposta da iteracao 0
+            }
 
         /**CALCULANDO QUANTAS LINHAS CADA PROCESSO RECEBERA**/
         int nlinhas, resto, ordem;
@@ -80,51 +72,108 @@ int main(int argc, char *argv[]){
         nlinhas = tam/(npes-1);
         resto = tam % (npes-1);
 
-
-        /**ENVIANDO AS LINHAS DAS MATRIZES A E b**/
+        /**ENVIANDO AS LINHAS DAS MATRIZES S**/
         int linhaidx = 0;
         msgtag = 1;
         ordem = nlinhas;
 
-		for (dest = 1; dest<npes-1; dest++){ //O resto da divisão de linhas será mandado para o último processo
+        for (dest = 1; dest<npes-1; dest++){ //O resto da divisao de linhas ser mandado para o ultimo processo
             for(i=0;i<ordem;i++){
                 MPI_Send(&S[linhaidx], tam+1, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
                 linhaidx++;
             }
-            MPI_Send(xk, tam, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
-		}
+                
+        }
 
-		ordem = nlinhas+resto;
-		dest = npes-1;
+        ordem = nlinhas+resto;
+        dest = npes-1;
         for(i=0;i<ordem;i++){ //Ultimo processo recebera o resto das linhas que sobraram
                 MPI_Send(&S[linhaidx], tam+1, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
                 linhaidx++;
         }
-        MPI_Send(xk, tam, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
 
-        /**RECEBENDO RESULTADO (xk_1)**/
-        ordem = nlinhas;
-        bufrecv = (double*)malloc((tam)*sizeof(double));
-        for(src=1;src<npes-1;src++){
-            MPI_Recv(bufrecv, ordem, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
-            for(i=(src-1)*ordem;i<src*ordem;i++){
-                xk[i] = bufrecv[i];
-                printf("%.1f ", xk[i]);
+        bufrecv = (double*)malloc((nlinhas+resto)*sizeof(double));
+        xk_1 = (double*)malloc((tam)*sizeof(double));
+        int count = 0;
+        
+        while(flag_parada == 0){
+            
+            /**ENVIANDO XK**/
+            MPI_Bcast(xk,tam,MPI_DOUBLE,0,MPI_COMM_WORLD);
+            
+            /**RECEBENDO RESULTADO (xk_1)**/
+            
+            count = 0;
+            for(src=1;src<(npes-1);src++){
+                MPI_Recv(bufrecv, nlinhas, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
+                for(i=0;i<nlinhas;i++){
+                    xk_1[count] = bufrecv[i];
+                    count++;
+                }
             }
+            MPI_Recv(bufrecv, nlinhas+resto, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
+            for(i=0;i<nlinhas+resto;i++){
+                    xk_1[count] = bufrecv[i];
+                    count++;
+                }
+
+            //Calculando precisao
+            for(i=0;i<tam;i++){
+                if(fabs(xk_1[i]-xk[i]) > precnum)
+                {
+                    precnum = fabs(xk_1[i]-xk[i]);
+                }
+
+                if(fabs(xk_1[i]) > precden)
+                {
+                    precden = fabs(xk_1[i]);
+                }         
+            }
+            precit = precnum/precden;
+            printf("erro = %.6f iteracao %d\n", precit,k);
+
+            if(precit > ERRO_ACEITAVEL){
+                flag_parada = 0;
+            }
+                
+           
+            if(precit < ERRO_ACEITAVEL){
+                flag_parada = 1;
+            }
+
+            
+
+            for(i=0;i<tam;i++){
+                xk[i] = xk_1[i];
+            }
+            precnum = precden = 0;
+            k++;
+            MPI_Bcast(&flag_parada,1,MPI_INT,0,MPI_COMM_WORLD);
         }
-        ordem = nlinhas+resto;
-        src = npes-1
-        MPI_Recv(bufrecv, ordem, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
-        for(i=(src-1)*nlinhas;i<src*ordem;i++){
-                xk[i] = bufrecv[i];
-                printf("%.1f ", xk[i]);
-            }
 
+         /**TESTE DA RESPOSTA**/
+        int equa;
+        double soma = 0;
+        printf("\n");
+        printf("qual equacao deseja utilizar para verificar resposta ? (considere comecando em 1)\n");
+        scanf("%d", &equa);
+        printf("\n");
+
+        equa--;
+        for(j=0;j<tam;j++)
+        {
+            soma += S[equa][j]*xk_1[j];
+        }
+
+        printf("resultado pelo metodo Gauss-Jacobi = %.3f\n", soma);
+        printf("valor de b%d = %.3f\n", equa+1, S[equa][tam]);
+        
 	}
 	else{
+
         /**CALCULANDO QUANTAS LINHAS CADA PROCESSO RECEBERA**/
         int nlinhas, resto, ordem;
-
+        double somaax = 0;
         nlinhas = tam/(npes-1);
         resto = tam % (npes-1);
 
@@ -133,7 +182,7 @@ int main(int argc, char *argv[]){
         else
             ordem = nlinhas+resto;
 
-        /**RECEBER AS LINHAS DAS MATRIZES A E b**/
+        /**RECEBER AS LINHAS DA MATRIZ S**/
         src = 0;
         msgtag = 1;
         bufrecv = (double*)malloc((tam+1)*sizeof(double));
@@ -142,47 +191,41 @@ int main(int argc, char *argv[]){
             MPI_Recv(bufrecv, tam+1, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
             for(j=0;j<tam+1;j++){
                 S[i][j] = bufrecv[j];
-                printf("%.1f ", S[i][j]);
             }
-            printf("S[%d](processo %d)\n",i, myrank);
         }
-
-        /**RECEBER xk**/
-        MPI_Recv(bufrecv, tam+1, MPI_DOUBLE, src, msgtag, MPI_COMM_WORLD, &status);
-        for(i=0;i<tam;i++){
-            xk[i] = bufrecv[i];
-            printf("%.1f ", xk[i]);
-        }
-
-        /**METODO DE GAUSS-JACOBI**/
-        int somaax;
-        double *xk_1;
         xk_1 = (double*)malloc(ordem*sizeof(double));
+        while(flag_parada == 0){
 
-        for(i=0;i<ordem;i++)
-        {
-            for(j=0;j<tam;j++)
+            MPI_Bcast(xk,tam,MPI_DOUBLE,0,MPI_COMM_WORLD);
+
+            /**METODO DE GAUSS-JACOBI**/
+            
+            
+            #pragma omp parallel for num_threads(n_threads) private(i,j) shared(S,tam,xk_1,xk) reduction(+:somaax)
+            for(i=0;i<ordem;i++)
             {
-                if(i != j){
-                    somaax += (xk[j]*S[i][j]);
-
+                somaax = 0;
+                for(j=0;j<tam;j++)
+                {
+                    if(j != ((myrank-1)*nlinhas)+i){
+                        somaax += (xk[j]*S[i][j]);
+                    }
                 }
+                xk_1[i] = (S[i][tam] - somaax)/S[i][((myrank-1)*nlinhas)+i];
             }
-            xk_1[i] = (S[i][tam] - somaax)/S[i][i];
+
+            /**ENVIAR RESULTADO PARA P0**/
+            int dest = 0;
+            MPI_Send(xk_1, ordem, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
+
+            MPI_Bcast(&flag_parada,1,MPI_INT,0,MPI_COMM_WORLD);
         }
-        printf("xk_1 = ");
-        for(i=0;i<ordem;i++)
-            printf("%.1f ", xk_1[i]);
-        printf("xk_1 (processo %d)\n", myrank);
-
-        /**ENVIAR RESULTADO PARA P0**/
-        dest = 0;
-        MPI_Send(xk_1, ordem, MPI_DOUBLE, dest, msgtag, MPI_COMM_WORLD);
-
-        free(xk_1);
+        
 	}
+
+    free(xk);
+    free(xk_1);
 	free(bufrecv);
-    printf("\n\n");
 	fflush(0);
 	ret = MPI_Finalize();
 	if (ret == MPI_SUCCESS)
